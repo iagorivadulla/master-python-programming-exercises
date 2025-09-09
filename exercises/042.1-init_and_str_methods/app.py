0046dc635ac9296c0b03adfc3dae060b0694f30b
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 18:24:08 +0000

    exercise 042.1

--- exercises/042.1-init_and_str_methods/app.py
+++ exercises/042.1-init_and_str_methods/app.py
@@ -1,2 +1,14 @@
 # Your code here
 
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"# Output: \n#\n# Book Title: {self.title}\n# Author: {self.author}\n# Year: {self.year}"
+
+book1 = Book("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+
+print(book1)
