b64f0e2131b4af215e64f878cbc48e24101c42df
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 14:45:43 +0000

    exercise 032

--- exercises/032-numbers-of-letters-and-digits/app.py
+++ exercises/032-numbers-of-letters-and-digits/app.py
@@ -1,1 +1,13 @@
 # Your code here
+def letters_and_digits(n):
+    exit = {'LETTERS' : 0, 
+            'DIGITS' : 0}
+    for i in n:
+        if i.isalpha():
+            exit['LETTERS'] += 1
+        elif i.isdigit():
+            exit['DIGITS'] += 1
+    
+    return exit
+
+print(letters_and_digits("hello world! 123"))
