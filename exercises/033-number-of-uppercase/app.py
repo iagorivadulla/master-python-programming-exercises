95b2e345d383997d8fee8217a414c0ec40838984
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 14:48:59 +0000

    exercise 033

--- exercises/033-number-of-uppercase/app.py
+++ exercises/033-number-of-uppercase/app.py
@@ -1,1 +1,13 @@
 # Your code here
+def number_of_uppercase(n):
+    exit = {'UPPERCASE' : 0, 
+            'LOWERCASE' : 0}
+    for i in n:
+        if i.isupper():
+            exit['UPPERCASE'] += 1
+        elif i.islower():
+            exit['LOWERCASE'] += 1
+    
+    return exit
+
+print(number_of_uppercase("Hello world!"))
