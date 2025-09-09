a5ff4a68cd877ebc8faa8d495c16a493c52ad961
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 18:03:13 +0000

    exercise 040

--- exercises/040-compute-robot-distance/app.py
+++ exercises/040-compute-robot-distance/app.py
@@ -1,1 +1,17 @@
 # Your code here
+def compute_robot_distance(n):
+    x = 0
+    y = 0
+    for i in n:
+        sep = i.split(' ')
+        if sep[0] == 'UP':
+            x += int(sep[1])
+        elif sep[0] == 'DOWN':
+            x -= int(sep[1])
+        elif sep[0] == 'RIGHT':
+            y += int(sep[1])
+        elif sep[0] == 'LEFT':
+            y -= int(sep[1])
+    return(x, y)
+
+print(compute_robot_distance(["UP 5", "DOWN 3", "LEFT 3", "RIGHT 2"]))
