ba358f3e2cc6528de3d8a8bedeb088e2261db9ea
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 17:16:07 +0000

    exercise 036

--- exercises/036-net-amount/app.py
+++ exercises/036-net-amount/app.py
@@ -1,1 +1,16 @@
 # Your code here
+def net_amount(n):
+    sep = n.split(' ')
+    nums = sep[1::2]
+    char = sep[0::2]
+    count = 0
+    for idx, i in enumerate(char):
+        if i == 'D':
+            count += int(nums[idx])
+        elif i == 'W':
+            count -= int(nums[idx])
+    
+    return count
+
+print(net_amount("D 300 D 300 W 200 D 100"))
+
