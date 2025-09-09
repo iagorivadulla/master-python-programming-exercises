4b5c1f9326fc3fb74ba6324ea43dc3462ff3f934
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 19:07:27 +0000

    exercise 045

--- exercises/045-class_methods/app.py
+++ exercises/045-class_methods/app.py
@@ -1,2 +1,12 @@
 # Your code here
 
+class MathOperations:
+    def __init__(self, pi=3.14159):
+        self.pi = pi
+
+    def calculate_circle_area(self, r):
+        return f"# Circle Area: {self.pi * r**2}"
+
+mops = MathOperations()
+circle_area = mops.calculate_circle_area(5)
+print(circle_area)
