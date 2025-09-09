7ffd0b71d14856d2a103f2f85252c7f681975fc5
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 17:38:48 +0000

    exercise 38

--- exercises/038-sort-tuples-ascending/app.py
+++ exercises/038-sort-tuples-ascending/app.py
@@ -1,3 +1,7 @@
 from operator import itemgetter
 
 # Your code here
+def sort_tuples_ascending(n):
+    return sorted(n, key=itemgetter(0, 1, 2))
+
+print(sort_tuples_ascending(['Tom,19,80', 'John,20,90', 'Jony,17,91', 'Jony,17,93', 'Jason,21,85']))
