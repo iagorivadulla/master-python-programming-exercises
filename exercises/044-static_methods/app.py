ac24047232bc84d8372ec12a17de2bc6300455ea
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 18:58:38 +0000

    exercise 044

--- exercises/044-static_methods/app.py
+++ exercises/044-static_methods/app.py
@@ -1,1 +1,12 @@
 # Your code here
+class MathOperations:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_numbers(n, m):
+        return f"# Sum of Numbers : {n + m}"
+    
+
+math_operations_instance = MathOperations()
+sum_of_numbers = print(MathOperations.add_numbers(10, 15))
