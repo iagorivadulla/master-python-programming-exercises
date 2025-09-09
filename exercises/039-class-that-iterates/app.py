6d1426887c7e39585444ae5dffb160c30a9f3cb1
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-09 17:45:19 +0000

    exercise 039

--- exercises/039-class-that-iterates/app.py
+++ exercises/039-class-that-iterates/app.py
@@ -1,1 +1,12 @@
 # Your code here
+class seven:
+    def __init__(self, n):
+        self.n = n
+
+    def generator(self):
+        for i in range(0, self.n + 1):
+            if i % 7 == 0:
+                yield i
+
+div = seven(50)
+print(list(div.generator()))
