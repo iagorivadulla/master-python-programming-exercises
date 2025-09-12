d24419849559aa5909532a335eb12f29a0a12603
Iago Rivadulla <iagorivadullaact@gmail.com>
2025-09-12 19:14:13 +0000

    changes

--- exercises/044-static_methods/app.py
+++ exercises/044-static_methods/app.py
@@ -1,8 +1,12 @@
 # Your code here
 class MathOperations:
-    def __init__(self):
-        pass
+    def __init__(self, pi = 3.141559):
+        self.pi = pi
+        
 
+    def calculate_circle_area(self, r):
+        return self.pi * (r ** 2)
+    
     @staticmethod
     def add_numbers(n, m):
         return f"# Sum of Numbers : {n + m}"
@@ -10,3 +14,7 @@ class MathOperations:
 
 math_operations_instance = MathOperations()
 sum_of_numbers = print(MathOperations.add_numbers(10, 15))
+
+print(math_operations_instance.calculate_circle_area(5))
+
+
